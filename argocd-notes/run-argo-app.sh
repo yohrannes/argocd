b0dfcffb7df6d6c848a3834f555fc0f1251c92e5
yohrannes <yohrannes@gmai.com>
2024-04-08 12:09:36 +0000

    test replicas argocd

---
+++ argocd-notes/run-argo-app.sh
@@ -0,0 +1,1 @@
+argocd app create port-dev-app --repo https://github.com/yohrannes/website-portifolio-argocd.git --path . --dest-server $(argocd cluster list | awk '{print $1}' | head -n 2 | tail -n 1) --dest-namespace default
