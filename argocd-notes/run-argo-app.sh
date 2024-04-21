09af68d4249ce3371a1cda9d5b7e1a849eece024
yohrannes <yohrannes@gmail.com>
2024-04-21 07:44:12 -0300

    atualizando script de instalação de deployment +notas

--- argocd-notes/run-argo-app.sh
+++ argocd-notes/run-argo-app.sh
@@ -1,1 +1,12 @@
-argocd app create port-dev-app --repo https://github.com/yohrannes/website-portifolio-argocd.git --path . --dest-server $(argocd cluster list | awk '{print $1}' | head -n 2 | tail -n 1) --dest-namespace default
+#!/bin/bash
+
+# Instalando repo no cluster
+
+argocd app create port-dev-app \
+--repo https://github.com/yohrannes/website-portifolio-argocd.git \
+--path . --dest-server $(argocd cluster list | awk '{print $1}' | head -n 2 | tail -n 1) \
+--dest-namespace yohapp
+
+# Listando status
+
+argocd app list
